e2017bc1fb004a0abe5eb7e02d93d995c1f6546d
Nacho Barrantes <40346406+Bagbundo99@users.noreply.github.com>
2023-01-09 03:29:55 +0000

    Modified start server, created script for userdata

--- startserver.sh
+++ startserver.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-source /home/$USER/project/webserver/venv/bin/activate
+source /home/$USER/project/venv/bin/activate
 cd /home/$USER/project/webserver/
+python3 manage.py migrate
 python3 manage.py runserver 127.0.0.1:5000  --insecure &
 
 cd /home/$USER/project/webserver1/
+python3 manage.py migrate
 python3 manage.py runserver 127.0.0.1:4999 --insecure &
 
 
---
+++ startup.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+#install reverse proxy 
+apt update 
+apt-get install -y nginx python3 python3-pip git python3-venv
+touch /etc/nginx/sites-enabled/error
+touch /etc/nginx/sites-enabled/principal
+
+#Modifies file 
+echo "server {
+    listen        80;
+    server_name error.nachosite.com;
+    location / {
+        proxy_pass         http://localhost:4999;
+        proxy_http_version 1.1;
+        proxy_set_header   Upgrade \$http_upgrade;
+        proxy_set_header   Connection keep-alive;
+        proxy_set_header   Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+        proxy_set_header   X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header   X-Forwarded-Proto \$scheme;
+    }
+}" > /etc/nginx/sites-enabled/error
+
+echo "server {
+    listen        80;
+    server_name principal.nachosite.com;
+    location / {
+        proxy_pass         http://localhost:5000;
+        proxy_http_version 1.1;
+        proxy_set_header   Upgrade \$http_upgrade;
+        proxy_set_header   Connection keep-alive;
+        proxy_set_header   Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+        proxy_set_header   X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header   X-Forwarded-Proto \$scheme;
+    }
+}" > /etc/nginx/sites-enabled/principal
+
+#start proxy
+systemctl start nginx
+systemctl enable nginx
+systemctl restart nginx
+
+#Creates server 
+mkdir /home/ubuntu/project
+cd /home/ubuntu/project
+python3 -m venv venv
+source /home/ubuntu/project/venv/bin/activate
+pip install Django django-cors-headers
+
+git clone https://github.com/Bagbundo99/DjangoHTTPLAB.git
+mv DjangoHTTPLAB/* .
+rm -rf DjangoHTTPLAB
+chown ubuntu:ubuntu -R /home/ubuntu/project 
+
+
+#starts server
+su ubuntu 
+cd /home/$USER/project
+./startserver.sh
+
+
+
+
+
