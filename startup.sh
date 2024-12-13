38bc2b6d8979a478f1f1a0406c79876348bfed59
nacho <40346406+Bagbundo99@users.noreply.github.com>
2024-12-13 15:19:16 -0600

    changes no sleep

--- startup.sh
+++ startup.sh
@@ -23,7 +23,14 @@ echo "server {
         proxy_pass         http://localhost:5000;
         proxy_http_version 1.1;
         proxy_set_header   Upgrade \$http_upgrade;
-     z
+        proxy_set_header   Connection keep-alive;
+        proxy_set_header   Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+        proxy_set_header   X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header   X-Forwarded-Proto \$scheme;
+    }
+}" > /etc/nginx/sites-enabled/principal
+
 
 #sed for the configuration file
 NGINX_CONF="/etc/nginx/nginx.conf"
